dddf5e44c37ef8545965e8f079b3ea1b64b7b212
John Chen <john.bm.chen@gmail.com>
2021-04-05 21:09:52 +1200

    added winning mechanics

--- script.js
+++ script.js
@@ -4,28 +4,35 @@ const ticGridStart = [2, 0, 0, 0, 0, 0, 0, 0, 0];
 var ticGrid = ticGridStart;
 var items = document.querySelectorAll(".GridItem");
 
-var isUserTurn = true;
+var isUserTurn = false;
+var gameWon = false;
 
 items.forEach(item =>  item.addEventListener('click', () => userTurn(item)));
 
 function userTurn(element){
     // console.log('hello');
     if (ticGrid[element.id] == 0 && isUserTurn == true){
-        ticGrid[element.id] = 1;
-        updateGrid();
-        isUserTurn = false;
+        updateGrid(element.id, 1);
+        // isUserTurn = false;
         botTurn();
     }
 }
 
 function botTurn(){
-    ticGrid[firstPossibleVal()] = 2;
-    updateGrid();
-    isUserTurn = true;
+    //add delay???
+    updateGrid(firstPossibleVal(), 2);
+    // isUserTurn = true;
 }
 
-function updateGrid(){
+function updateGrid(index, val){
+    if (gameWon == true){
+        return;
+    }
+    
+    ticGrid[index] = val;
     items.forEach(item => item.innerHTML = symbolfy(ticGridStart[item.id]))
+    isUserTurn = (isUserTurn == true) ? false : true;
+    checkWin();
 }
 
 function symbolfy(num){
@@ -37,6 +44,29 @@ function firstPossibleVal(){
         if (ticGrid[index] == 0){
             return index;
         }
+    }
+}
+
+function checkWin(){
+    for (var num = 0; num <= 3; num++){
+        //boxed
+        //1,2,3
+        var twoNum = num * 2;
+        checkTuple(ticGrid[twoNum + 1], ticGrid[twoNum + 2], (ticGrid[twoNum + 3] > 8) ? ticGrid[1]: ticGrid[twoNum + 3]);
+
+        //centred
+        checkTuple(ticGrid[0], ticGrid[num + 1], ticGrid[num + 5]);
+    }
+}
+
+function checkTuple(num1, num2, num3){
+    var tuple = [num1, num2, num3];
+
+    if (tuple[1] == tuple[0] && tuple[1] == tuple[2] && tuple[0] != 0){
+        //win!!
+        console.log('Win');
+        console.log(tuple);
+        gameWon = true;
     }
 }
 
