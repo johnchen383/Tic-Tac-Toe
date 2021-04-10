d7d4cd3058b151e30ee0e34e4a596164fe2f671c
John Chen <john.bm.chen@gmail.com>
2021-04-10 22:07:14 +1200

    finished definite move functionality

--- script.js
+++ script.js
@@ -20,7 +20,15 @@ function userTurn(element){
 
 function botTurn(){
     //add delay???
-    updateGrid(firstPossibleVal(), 2);
+
+    var indexVal = definiteMove();
+    // var indexVal;
+
+    if (indexVal == null){
+        indexVal = firstPossibleVal();
+    }
+
+    updateGrid(indexVal, 2);
     // isUserTurn = true;
 }
 
@@ -52,15 +60,16 @@ function checkWin(){
         //boxed
         //1,2,3
         var twoNum = num * 2;
-        checkTuple(ticGrid[twoNum + 1], ticGrid[twoNum + 2], (ticGrid[twoNum + 3] > 8) ? ticGrid[1]: ticGrid[twoNum + 3]);
+        checkTupleWin(ticGrid[twoNum + 1], ticGrid[twoNum + 2], (twoNum + 3 > 8) ? ticGrid[1]: ticGrid[twoNum + 3]);
 
         //centred
-        checkTuple(ticGrid[0], ticGrid[num + 1], ticGrid[num + 5]);
+        checkTupleWin(ticGrid[0], ticGrid[num + 1], ticGrid[num + 5]);
     }
 }
 
-function checkTuple(num1, num2, num3){
+function checkTupleWin(num1, num2, num3){
     var tuple = [num1, num2, num3];
+    // console.log(tuple)
 
     if (tuple[1] == tuple[0] && tuple[1] == tuple[2] && tuple[0] != 0){
         //win!!
@@ -68,6 +77,80 @@ function checkTuple(num1, num2, num3){
         console.log(tuple);
         gameWon = true;
     }
+}
+
+function definiteMove(){
+    // console.log('definitie moveeeeeeeeeeeeeeeeeeee............')
+    var hasInstantWin = true;
+    for (var num = 0; num <= 3; num++){
+        //boxed
+        //1,2,3
+        var twoNum = num * 2;
+        var temp;
+        temp = checkTupleDefinite(twoNum + 1, twoNum + 2, (twoNum + 3 > 8) ? 1 : twoNum + 3, hasInstantWin);
+        if (temp != null){
+            return temp;
+        }
+        //centred
+        temp = checkTupleDefinite(0, num + 1, num + 5, hasInstantWin);
+        if (temp != null){
+            return temp;
+        }
+    }
+
+    hasInstantWin = false;
+    for (var num = 0; num <= 3; num++){
+        //boxed
+        //1,2,3
+        var twoNum = num * 2;
+        var temp;
+        temp = checkTupleDefinite(twoNum + 1, twoNum + 2, (twoNum + 3 > 8) ? 1 : twoNum + 3, hasInstantWin);
+        if (temp != null){
+            return temp;
+        }
+        //centred
+        temp = checkTupleDefinite(0, num + 1, num + 5, hasInstantWin);
+        if (temp != null){
+            return temp;
+        }
+    }
+
+    return null;
+}
+
+function checkTupleDefinite(num1, num2, num3, winCheck){
+    // console.log(num1 + ' ' + num2 + ' ' + num3)
+    var tuple = [ticGrid[num1], ticGrid[num2], ticGrid[num3]];
+
+    // console.log('Checking tuple: ' + tuple);
+    
+    var zeroIndex;
+    if (ticGrid[num1] == 0){
+        zeroIndex = num1;
+    } else if (ticGrid[num2] == 0){
+        zeroIndex = num2;
+    } else if (ticGrid[num3] == 0){
+        zeroIndex = num3;
+    }
+
+    // console.log('zero index is: ' + zeroIndex);
+
+    tuple.sort();
+
+    if (tuple[0] == 0 && tuple[1] == tuple[2] && tuple[1] != 0){
+        //definite move... return index
+        if (winCheck == false){
+            return zeroIndex;
+        } else {
+            if (tuple[1] == 2){
+                // console.log('INSTANT WIN')
+                return zeroIndex;
+            }
+        }
+        
+    }
+
+    return null;
 }
 
 //game begins
